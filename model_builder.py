0592fd77ed7d01461240eefed921301a8ddd4b06
Junaid Siddiqui <junaidas97@gmail.com>
2023-12-29 15:57:36 -0500

    adding 2 outputs

--- model_builder.py
+++ model_builder.py
@@ -6,12 +6,18 @@ import sql_manager
 import sql_queries
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import *
+from tensorflow.keras.layers import InputLayer
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import RNN
+from tensorflow.keras.layers import CNN
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.losses import MeanSquaredError
 from tensorflow.keras.metrics import RootMeanSquaredError
 from tensorflow.keras.optimizers import Adam
 
+
+from sklearn.metrics import mean_squared_error as mse
 from sklearn.model_selection import train_test_split
 
 import matplotlib.pyplot as plt
@@ -38,12 +44,33 @@ def str_to_datetime(s):
     year, month, day = int(split[0]), int(split[1]), int(split[2])
     return datetime.datetime(year=year, month=month, day=day)
 
+def windowed_df_to_date_X_y(windowed_dataframe):
+    df_as_np = windowed_dataframe.to_numpy()
+    
+    print()
+    
+    dates = df_as_np[:, 0]
+
+    middle_matrix = df_as_np[:, 2:]
+    X = middle_matrix.reshape((len(dates), middle_matrix.shape[1], 1))
+
+    Y = df_as_np[:, 1]
+
+    return dates, X.astype(np.float32), Y.astype(np.float32)
 
 def split_data():
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=5)
-    return train,
+    return X_train, X_test, y_train, y_test
 
+def build_LSTM():
+    model5 = Sequential()
+    model5.add(InputLayer((7, 6)))
+    model5.add(LSTM(64))
+    model5.add(Dense(8, 'relu'))
+    model5.add(Dense(2, 'linear'))
+
+    model5.summary()
 
 if __name__ == '__main__':
 
@@ -65,6 +92,12 @@ if __name__ == '__main__':
 
     nvdia_ts = nvdia_ts.drop('Seconds', axis=1)
     # ML stuff
+
+    dates, X, y = windowed_df_to_date_X_y(nvdia_ts)
+
+    print(dates)
+
+    exit()
 
     # plt.plot(np.array(nvdia_ts['Day sin'])[:200])
     # plt.plot(np.array(nvdia_ts['Day cos'])[:200])
@@ -99,8 +132,3 @@ if __name__ == '__main__':
 # time_series.ipynb
 # https://colab.research.google.com/github/tensorflow/docs/blob/master/site/en/tutorials/structured_data/time_series.ipynb#scrollTo=6GmSTHXw6lI1
 
-# Microsoft Stock Forecasting with LSTMs.ipynb
-# https://colab.research.google.com/drive/1Bk4zPQwAfzoSHZokKUefKL1s6lqmam6S
-
-# Amazon Stock Forecasting with LSTM.ipynb
-# https://colab.research.google.com/drive/1dYAxxrJNx8L0yF_pLboXeiwvt1Txddsb#scrollTo=pXHkuFeafNUU
